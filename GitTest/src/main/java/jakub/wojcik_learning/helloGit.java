710703daa1138a27b66e26d0df9f447bc6292601
Jakub Wójcik <wujek444@gmail.com>
2017-01-12 01:14:03 +0100

    added String variable

--- GitTest/src/main/java/jakub/wojcik_learning/helloGit.java
+++ GitTest/src/main/java/jakub/wojcik_learning/helloGit.java
@@ -3,7 +3,8 @@ package jakub.wojcik_learning;
 public class helloGit {
 //a new line
 	public static void main(String[] args) {
-		System.out.println("Hello GIT! What a nice day to be a developer!");
+		String text = "Hello GIT! What a nice day to be a developer!";
+		System.out.println(text);
 
 	}
 
