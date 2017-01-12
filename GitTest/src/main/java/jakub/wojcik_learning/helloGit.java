a0029e2b70d9b25443e6a53cb377b87fbc8efd32
Jakub Wójcik <wujek444@gmail.com>
2017-01-12 01:12:46 +0100

    changed text

--- GitTest/src/main/java/jakub/wojcik_learning/helloGit.java
+++ GitTest/src/main/java/jakub/wojcik_learning/helloGit.java
@@ -3,7 +3,7 @@ package jakub.wojcik_learning;
 public class helloGit {
 //a new line
 	public static void main(String[] args) {
-		System.out.println("Hello GIT!");
+		System.out.println("Hello GIT! Hello team!");
 
 	}
 
