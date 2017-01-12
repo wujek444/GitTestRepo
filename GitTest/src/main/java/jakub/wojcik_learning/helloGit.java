3ae1e7e5e954d86f0c942c6261417c2a758ed269
Jakub Wójcik <wujek444@gmail.com>
2017-01-12 01:10:40 +0100

    Modified output text

--- GitTest/src/main/java/jakub/wojcik_learning/helloGit.java
+++ GitTest/src/main/java/jakub/wojcik_learning/helloGit.java
@@ -3,7 +3,7 @@ package jakub.wojcik_learning;
 public class helloGit {
 //a new line
 	public static void main(String[] args) {
-		System.out.println("Hello GIT!");
+		System.out.println("Hello GIT! What a nice day to be a developer!");
 
 	}
 
